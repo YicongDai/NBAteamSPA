703eebabe199f7e748d2d158ca674e6a8ff6ba43
Yicong Dai <252332082@qq.com>
2018-12-02 21:28:04 +0800

    initial structure

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Teams from '@/components/Teams'
+import AddTeam from '@/components/Addteam'
+import AboutUs from '@/components/AboutUs'
+import ContactUs from '@/components/ContactUs'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/teams',
+      name: 'Teams',
+      component: Teams
+    },
+    {
+      path: '/addTeam',
+      name: 'AddTeam',
+      component: AddTeam
+    }, {
+      path: '/about',
+      name: 'AboutUs',
+      component: AboutUs
+    },
+    {
+      path: '/contact',
+      name: 'ContactUs',
+      component: ContactUs
+    }
+  ]
+})
