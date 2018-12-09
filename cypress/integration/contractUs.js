6356006a61858d499689392a203af109edb2e1b9
Yicong Dai <252332082@qq.com>
2018-12-10 05:25:32 +0800

    contract us spe tested

--- cypress/integration/contractUs.js
+++ cypress/integration/contractUs.js
@@ -1,11 +1,11 @@
-describe('About us page', () => {
+describe('Contract us page', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.get('.navbar-nav:nth-child(2)')
-      .find('.nav-item:nth-child(1)').click()
+      .find('.nav-item:nth-child(2)').click()
   })
   it('show some information about the app', () => {
-    cy.get('.vue-title').contains('About Us')
+    cy.get('.vue-title').contains('Contract Us')
     cy.get('.donation-footer').contains('here')
   })
 })
